cd5d50a0f643e7069bc6c1e26ecc50471b1e6241
Yash-Tibadia <tibadiyayash@gmail.com>
2025-10-10 20:55:38 +0530

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,16 +9,6 @@ const config: Config = {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         lora: ["Lora", ...defaultTheme.fontFamily.serif],
       },
-      fontSize: {
-        xs: ["0.75rem", { lineHeight: "1rem" }], // 12px
-        sm: ["0.875rem", { lineHeight: "1.25rem" }], // 14px
-        base: ["1rem", { lineHeight: "1.75rem" }], // 16px
-        lg: ["1.125rem", { lineHeight: "1.75rem" }], // 18px
-        xl: ["1.25rem", { lineHeight: "1.75rem" }], // 20px
-        "2xl": ["1.5rem", { lineHeight: "2rem" }], // 24px
-        "3xl": ["1.875rem", { lineHeight: "2.25rem" }], // 30px
-        "4xl": ["2.25rem", { lineHeight: "2.5rem" }], // 36px
-      },
       colors: {
         primary: {
           50: "#F3F5F2",
